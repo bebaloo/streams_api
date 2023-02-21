8900a57a009073a1920259c2c85a7d538a895ceb
Dmytro Tkachuk <geekhubme@gmail.com>
2023-02-21 16:15:22 +0200

    Fix previous commit

--- src/main/java/com/foxminded/service/ResultGenerator.java
+++ src/main/java/com/foxminded/service/ResultGenerator.java
@@ -1,17 +1,18 @@
 package com.foxminded.service;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import com.foxminded.model.AbbreviationData;
+import com.foxminded.model.EndData;
+import com.foxminded.model.ResultLap;
+import com.foxminded.model.StartData;
+import com.foxminded.service.parser.AbbreviationDataParseService;
+import com.foxminded.service.parser.EndDataParseService;
+import com.foxminded.service.parser.StartDataParseService;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-
-import com.foxminded.model.*;
-import com.foxminded.service.parser.AbbreviationDataParseService;
-import com.foxminded.service.parser.EndDataParseService;
-import com.foxminded.service.parser.StartDataParseService;
 
 public class ResultGenerator {
     private final String FILE_PATH_ABBREVIATIONS = "src/main/resources/abbreviations.txt";
@@ -27,29 +28,25 @@ public class ResultGenerator {
     public List<ResultLap> generate() {
         List<ResultLap> results = new ArrayList<>();
 
-        try {
-            List<StartData> startLogs = startDataParseService.parse();
-            List<EndData> endLogs = endDataParseService.parse();
-            List<AbbreviationData> abbreviations = abbreviationDataParseService.parse();
+        List<StartData> startLogs = startDataParseService.parse();
+        List<EndData> endLogs = endDataParseService.parse();
+        List<AbbreviationData> abbreviations = abbreviationDataParseService.parse();
 
-            for (StartData startData : startLogs) {
-                for (EndData endData : endLogs) {
-                    for (AbbreviationData abbreviationData : abbreviations) {
-                        if (startData.getRacerAbbreviation().equals(endData.getRacerAbbreviation()) && startData.getRacerAbbreviation().equals(abbreviationData.getRacerAbbreviation())) {
-                            LocalDateTime startTime = startData.getRacerStartTime();
-                            LocalDateTime endTime = endData.getRacerEndTime();
+        for (StartData startData : startLogs) {
+            for (EndData endData : endLogs) {
+                for (AbbreviationData abbreviationData : abbreviations) {
+                    if (startData.getRacerAbbreviation().equals(endData.getRacerAbbreviation()) && startData.getRacerAbbreviation().equals(abbreviationData.getRacerAbbreviation())) {
+                        LocalDateTime startTime = startData.getRacerStartTime();
+                        LocalDateTime endTime = endData.getRacerEndTime();
 
-                            Duration timeLap = Duration.between(startTime, endTime);
-                            String racerName = abbreviationData.getRacerName();
-                            String racerTeam = abbreviationData.getRacerTeam();
+                        Duration timeLap = Duration.between(startTime, endTime);
+                        String racerName = abbreviationData.getRacerName();
+                        String racerTeam = abbreviationData.getRacerTeam();
 
-                            results.add(new ResultLap(racerName, racerTeam, timeLap));
-                        }
+                        results.add(new ResultLap(racerName, racerTeam, timeLap));
                     }
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
         results.sort(Comparator.comparing(ResultLap::getTimeLap));
